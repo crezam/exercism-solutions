5256338a793220fc1328093b26eff68b56b592d6
Camilo Crespo <crezam@live.com>
2018-02-09 23:04:30 -0500

    Go: scrabble solution v2

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,12 +1,16 @@
 package scrabble
 
-import "unicode"
+import (
+	"unicode"
+	"strings"
+)
 
-//abcdefghijklmnopqrstuvwxyz
+// Stores in contiguous memory the numeric value equivalent to the character as per its position in the sequence "abcdefghijklmnopqrstuvwxyz"
 var charValues = [...]uint8{1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
 
 // Score returns Scrabble score of string
 func Score(input string) int {
+	input = strings.ToLower(input)
 	var score int
 	for _, r := range input {
 		if unicode.IsLetter(r) {
