806fefe79b9266c81eaa3cb4ce1db2405937a203
Camilo Crespo <crezam@live.com>
2018-03-12 00:34:59 -0400

    Go: grains solution v2

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -8,9 +8,11 @@ import (
 // Square returns the number of grains in the input square position (starting by !)
 func Square(input int) (actualVal uint64, actualErr error) {
 	if input < 1 || input > 64 {
-		return 0, errors.New("Input should be between 1 and 64 inclusive")
+		actualVal, actualErr = 0, errors.New("input should be between 1 and 64 inclusive")
+	} else {
+		actualVal, actualErr = uint64(math.Pow(float64(2), float64(input-1))), nil
 	}
-	return uint64(math.Pow(float64(2), float64(input-1))), nil
+	return
 }
 
 // Total returns the total number of grains in a chess board
